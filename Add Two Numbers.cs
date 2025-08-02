abacab1df698c160bd5e8908594c9c66ebdcbe0d
Patryk Bajak <Patryk.Bajak@microsoft.wsei.edu.pl>
2025-08-02 13:46:38 +0200

    wip

---
+++ Add Two Numbers.cs
@@ -0,0 +1,150 @@
+﻿//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+
+//Example 1:
+
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+
+//Example 2:
+
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+
+//Example 3:
+
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+
+
+
+//Constraints:
+
+//    The number of nodes in each linked list is in the range [1, 100].
+//    0 <= Node.val <= 9
+//    It is guaranteed that the list represents a number that does not have leading zeros.
+
+//You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+//You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+
+
+//Example 1:
+
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+
+//Example 2:
+
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+
+//Example 3:
+
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+
+
+
+//Constraints:
+
+//    The number of nodes in each linked list is in the range [1, 100].
+//    0 <= Node.val <= 9
+//    It is guaranteed that the list represents a number that does not have leading zeros.
+
+
+
+using System.Text;
+
+namespace Algorithms
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    internal class Add_Two_Numbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var firstNumber = CreateNumberFromList(l1);
+            var secondNumber = CreateNumberFromList(l2);
+
+            var sum = AddTwoBigNumbers(firstNumber, secondNumber);
+
+            ListNode head = null;
+            ListNode current = null;
+
+            for(int i = sum.Length - 1; i >= 0; i--)
+            {
+                var node = new ListNode(int.Parse(sum[i].ToString()));
+                if(head == null)
+                {
+                    head = current = node;
+                }
+                else
+                {
+                    current.next = node;
+                    current = node;
+                }
+            }
+            return head;
+        }
+
+        private string CreateNumberFromList(ListNode l)
+        {
+            var result = string.Empty;
+            var current = l;
+
+            while(current.next != null)
+            {
+                result += current.val.ToString();
+                current = current.next;
+            }
+
+            result += current.val.ToString();
+            return string.Join("", result.Reverse());
+        }
+
+        private string AddTwoBigNumbers(string first, string second)
+        {
+            int rest = 0;
+            int maxLength = Math.Max(first.Length, second.Length);
+
+            var result = new StringBuilder();
+
+            first = first.PadLeft(maxLength, '0');
+            second = second.PadLeft(maxLength, '0');
+
+            for (int i = maxLength - 1; i >= 0; i--)
+            {
+                var firstNumber = first[i] - '0';
+                var secondNumber = second[i] - '0';
+
+                var sum = firstNumber + secondNumber + rest;
+                rest = sum / 10;
+
+                result.Insert(0, (sum % 10).ToString());
+            }
+
+            if (rest > 0)
+            {
+                result.Insert(0, rest.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -44,9 +44,34 @@
             //var resultt = result.IncreasingTriplet(new int[] {2,4,-2,-3,});
             //Console.WriteLine(resultt);
 
-            Count__adn_Say result = new Count__adn_Say();
+            ListNode l11 = new ListNode(4);
+            ListNode l12 = new ListNode(1);
+            ListNode l13 = new ListNode(8);
+            ListNode l14 = new ListNode(4);
 
-            Console.WriteLine(result.CountAndSay(5));
+            l11.next = l12;
+            l12.next = l13;
+            l13.next = l14;
+
+            ListNode l21 = new ListNode(5);
+            ListNode l22 = new ListNode(6);
+            ListNode l23 = new ListNode(1);
+            ListNode l24 = new ListNode(8);
+            ListNode l25 = new ListNode(4);
+
+            l21.next = l22;
+            l22.next = l23;
+            l23.next = l24;
+            l24.next = l25;
+
+            //Add_Two_Numbers res = new Add_Two_Numbers();
+
+            //res.AddTwoNumbers(l11, l21);
+
+            Intersection_of_Two_Linked_Lists result = new Intersection_of_Two_Linked_Lists();
+            var res =result.GetIntersectionNode(l11, l21);
+            Console.WriteLine();
+
         }
     }
 }
