b9ec5d1d4ecc8a965092d266fa2b171b3062903c
Ptrk12 <random12@onet.pl>
2025-08-07 18:29:08 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -6,21 +6,24 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            TreeNode root = new TreeNode(1);
-            TreeNode t1 = new TreeNode(2);
-            TreeNode t2 = new TreeNode(3);
-            TreeNode t3 = new TreeNode(4);
-            TreeNode t4 = new TreeNode(5);
+            TreeNode root = new TreeNode(5);
+            TreeNode n1 = new TreeNode(3);
+            TreeNode n2 = new TreeNode(6);
+            TreeNode n3 = new TreeNode(2);
+            TreeNode n4 = new TreeNode(4);
+            TreeNode n5 = new TreeNode(1);
 
-            root.left = t1;
-            root.right = t2;
-            t1.left = t3;
-            t1.right = t4;
+            root.left = n1;
+            root.right = n2;
+            n1.right = n4;
+            n1.left = n3;
+            n3.left = n5;
 
-            Binary_Tree_Zigzag_Level_Order_Traversal zigzag = new Binary_Tree_Zigzag_Level_Order_Traversal();
-            var res = zigzag.ZigzagLevelOrder(root);
+            Kth_Smallest_Element_in_a_BST test = new Kth_Smallest_Element_in_a_BST();
 
-            Console.WriteLine();
+           var res = test.KthSmallest(root,3);
+            Console.WriteLine(res);
+
 
         }
     }
---
+++ Trees and graphs/Kth Smallest Element in a BST.cs
@@ -0,0 +1,64 @@
+﻿//Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+//Example 1:
+
+//Input: root = [3, 1, 4, null, 2], k = 1
+//Output: 1
+
+//Example 2:
+
+//Input: root = [5, 3, 6, 2, 4, null, null, 1], k = 3
+//Output: 3
+
+
+
+
+//Constraints:
+
+//The number of nodes in the tree is n.
+//    1 <= k <= n <= 104
+//    0 <= Node.val <= 104
+
+ 
+
+//Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+
+namespace Algorithms.Trees_and_graphs
+{
+    internal class Kth_Smallest_Element_in_a_BST
+    {
+        private TreeNode _root;
+        private int counter = 1;
+        private int result = -1;
+        public int KthSmallest(TreeNode root, int k)
+        {
+            InOrderTraversal(root, k);
+            return result;
+        }
+        private void InOrderTraversal(TreeNode root, int k)
+        {
+            if(_root == null)
+                _root = root;
+            InOrderTraversalRec(root,k);
+        }
+
+        private void InOrderTraversalRec(TreeNode root, int k)
+        {
+            if(root != null && result == -1 )
+            {
+                InOrderTraversalRec(root.left,k);
+                if (k == counter)
+                {
+                    result = root.val;
+                }
+                    
+                counter++;
+                InOrderTraversalRec(root.right,k);
+
+            }
+        }
+    }
+}
